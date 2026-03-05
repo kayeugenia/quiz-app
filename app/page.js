aec22bdca96fe0f8a74d4cb7cec120477a5020c8
My Tran <133477718+aemeeeee@users.noreply.github.com>
2026-03-05 23:33:03 +0800

    Fix spacing error in disclaimer text

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
           <button>Start Quiz</button>
         </Link>
         <div className="disclaimer disclaimer-bold" style={{textAlign: 'center', paddingBottom: '5px'}}>Disclaimer</div>
-        <div className="disclaimer" style={{textAlign: 'justify', padding: '0 50px 0 50px'}}>This quiz poses short questions that help locate courses that align with your interests. All courses collectively promote <strong>(1)critical thinking</strong>, <strong>(2) an appreciation of complexity</strong>, <strong>(3) effective communication</strong> and <strong>(4) personal and social responsibility</strong>. They meet three General Education requirements <strong>as a block</strong>. The final courses returned may not be an absolutely perfect fit for you but they should connect with at least some of your interests.</div>
+        <div className="disclaimer" style={{textAlign: 'justify', padding: '0 50px 0 50px'}}>This quiz poses short questions that help locate courses that align with your interests. All courses collectively promote <strong>(1) critical thinking</strong>, <strong>(2) an appreciation of complexity</strong>, <strong>(3) effective communication</strong> and <strong>(4) personal and social responsibility</strong>. They meet three General Education requirements <strong>as a block</strong>. The final courses returned may not be an absolutely perfect fit for you but they should connect with at least some of your interests.</div>
       </div>
     </main>
   );
